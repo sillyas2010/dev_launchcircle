572a628dd979a5ed51b861f93a683c83a26d2a42
Oleg Stupak <oleg@mgroupweb.com>
2017-08-23 17:49:24 +0300

    Home page

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+$('#launchCircleCarousel').carousel({
+    interval: 10000000,
+    pause: 'hover'
+})
+
+
+$('#launchCircleToptalent').carousel({
+    interval: 10000900,
+    pause: 'hover'
+})
+
+
