0fdf62993acd4f67448e4d5241cdf4bd7cfaad44
Oleg Stupak <oleg@mgroupweb.com>
2017-08-25 16:57:41 +0300

    Home page, about us page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,56 @@
+// CUSTOM SCRIPTS
+
+(function ($) {
+
+    "use strict";
+
+    // *** init slider
+    $('#launchCircleCarousel').carousel({
+        interval: 10000000,
+        pause: 'hover'
+    })
 
 
-$('#launchCircleCarousel').carousel({
-    interval: 10000000,
-    pause: 'hover'
-})
+    $('#launchCircleToptalent').carousel({
+        interval: 10000900,
+        pause: 'hover'
+    })
 
 
-$('#launchCircleToptalent').carousel({
-    interval: 10000900,
-    pause: 'hover'
-})
+
+    $('a.page-scroll').on('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 61
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+
+
+
+    //Scrollspy offset
+    $(window).on('scroll', function(event) {
+        var scrollValue = $(window).scrollTop();
+        var nav = $("#thing"),  scroll = $(".page-scroll"), navBar = nav.find('.navbar');
+
+        if(scrollValue > 70){
+            nav.addClass('stuck');
+            navBar.addClass('container');
+        } else {
+            nav.removeClass('stuck');
+            navBar.removeClass('container');
+        }
+        scrollValue > 600 ? scroll.addClass('active') : scroll.removeClass('active');
+    });
+
+})(jQuery);
+
+
+
+
+
+
+
 
 
