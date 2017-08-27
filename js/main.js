c5de7476b1462a8fb81814d5e87b3f17fe67844c
Oleg Stupak <oleg@mgroupweb.com>
2017-08-27 11:34:41 +0300

    Home page, about us page

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@
     $('a.page-scroll').on('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 61
+            scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
