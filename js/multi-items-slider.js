0b358282a3c33df97034d7af3a06ce38b91f6038
Oleg Stupak <sillyas2010@mgroupweb.com>
2017-08-31 16:01:04 +0300

    lyke page, changed slider

--- js/multi-items-slider.js
+++ js/multi-items-slider.js
@@ -1,19 +1,21 @@
-$('.carousel-multi').on('slide.bs.carousel', function (e) {
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 3;
-    var totalItems = $('.carousel-multi .carousel-item').length;
-
-    if (idx >= totalItems-(itemsPerSlide-1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
-            // append slides to end
-            if (e.direction=="left") {
-                $('.carousel-multi .carousel-item').eq(i).appendTo('.carousel-multi .carousel-inner');
+// *** init slider
+$('#launchCircleCarousel .carousel-inner').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: $('.carousel-control-next'),
+    prevArrow: $('.carousel-control-prev'),
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2
             }
-            else {
-                $('.carousel-multi .carousel-item').eq(0).appendTo('.carousel-multi .carousel-inner');
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
             }
         }
-    }
+    ]
 });
