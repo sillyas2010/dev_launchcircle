f1b774d03f14a7adb192bb1512288c766f081d71
Oleg Stupak <sillyas2010@gmail.com>
2017-09-19 11:08:47 +0300

    doing faq-page

--- js/multi-items-slider.js
+++ js/multi-items-slider.js
@@ -1,21 +1,49 @@
 // *** init slider
-$('.slick-slider.carousel-multi .carousel-inner').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    nextArrow: $('.slick-slider .carousel-control-next'),
-    prevArrow: $('.slick-slider .carousel-control-prev'),
-    responsive: [
-        {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 2
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1
-            }
-        }
-    ]
+$(document).ready(function () {
+    if(!$('.carousel-center').length) {
+        $('.slick-slider.carousel-multi .carousel-inner').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            nextArrow: $('.slick-slider .carousel-control-next'),
+            prevArrow: $('.slick-slider .carousel-control-prev'),
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        });
+    }
+    else {
+        $('.carousel-center .carousel-inner').slick({
+            centerMode: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            nextArrow: $('.slick-slider .carousel-control-next'),
+            prevArrow: $('.slick-slider .carousel-control-prev'),
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        centerMode:true,
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        });
+    }
 });
