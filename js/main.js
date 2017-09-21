6693097607357cd54b0f1147ff293e5d32d9fce0
Oleg Stupak <sillyas2010@gmail.com>
2017-09-21 17:00:53 +0300

    doing german translations(portfolio-page)

--- js/main.js
+++ js/main.js
@@ -173,6 +173,7 @@
             lang = lang || undefined;
 
         if(!isInit && ifLangChanged() && lang === 'en') {
+            debugger;
             var startsFrom = currPage.matches(langExists).index,
                 newPage = currPage.matches(langExists).input.substring(0,startsFrom);
             window.location.href = newPage;
