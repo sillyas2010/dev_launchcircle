5d61cdcb9603fe48f68e658f3b9f101a77720536
Oleg Stupak <sillyas2010@gmail.com>
2017-10-05 17:55:26 +0300

    fix change lang on faq page

--- js/main.js
+++ js/main.js
@@ -65,7 +65,7 @@
         }
         //Temporary action, while faq page still haven't german translations
         else if(!/(faq)/g.test(window.location.pathname)) {
-            $(this).val(0).change();
+            $(this).val(0);
         }
     });
 
